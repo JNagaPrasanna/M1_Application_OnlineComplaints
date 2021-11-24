7ecea025525a19a01443252f9dd9d3acfc9e81f4
JNagaPrasanna <94163912+JNagaPrasanna@users.noreply.github.com>
2021-11-24 21:35:58 +0530

    Create online_complaint.h

---
+++ 3_Implementation/inc/online_complaint.h
@@ -0,0 +1,28 @@
+**
+ * @file online_complaint.h
+ * @author Prasanna
+ * @brief online complaint structure definition
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ * 
+ */
+
+#ifndef __ONLINE_COMPLAINT_H__
+#define __ONLINE_COMPLAINT_H__
+
+
+#include<string.h>
+struct complaint
+{
+	char number[6];
+	char name[15];
+    char problem[15];
+	int complaint_num;
+	char local_address[15];
+	struct complaint *following;
+}
+
+#endif //#ifndef ONLINE_COMPLAINT
